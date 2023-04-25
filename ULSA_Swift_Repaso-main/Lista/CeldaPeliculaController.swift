45f8ed39257b10953df40782c68187b02952a42f
gpablosl <69810373+gpablosl@users.noreply.github.com>
2023-04-24 19:26:05 -0700

    Add files via upload

---
+++ ULSA_Swift_Repaso-main/Lista/CeldaPeliculaController.swift
@@ -0,0 +1,17 @@
+//
+//  CeldaPeliculaController.swift
+//  Lista
+//
+//  Created by Alumno ULSA on 24/04/23.
+//
+
+import Foundation
+
+import UIKit
+
+class CeldaPeliculaController : UITableViewCell {
+    
+    @IBOutlet weak var lblTitulo: UILabel!
+    @IBOutlet weak var lblDirector: UILabel!
+    @IBOutlet weak var lblAño: UILabel!
+}
---
+++ ULSA_Swift_Repaso-main/README.md
@@ -0,0 +1,1 @@
+# ULSA_Swift_Repaso
